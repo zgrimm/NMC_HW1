6ea964a1137dbdac0f826e951ce7a9a74fbacd3a
Zach Home <zwgrimm@gmail.com>
2018-12-20 02:26:09 -0800

    added some duck typing to index.ts

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,10 +1,8 @@
 /// <reference path="../ref/index.d.ts" />
 
-/*
-**
-** Entry Point for App, starts up the server
-**
-*/
+/**********************************************
+** Entry Point for App, starts up the server **
+**********************************************/
 
 // Dependencies
 import * as http from 'http';
@@ -15,34 +13,36 @@ import * as String_Decoder from 'string_decoder';
 const StringDecoder = String_Decoder.StringDecoder;
 
 //Interfaces
-import { MethodHandlers } from './interfaces';
-import { RequestData } from './interfaces';
+import { MethodHandler, RequestData } from './interfaces';
+import { IncomingMessage, ServerResponse } from 'http';
+import { ServerOptions } from 'https';
 
 export class AppServer {
 
 	private httpServer;
-	private httpsServerOptions;
 	private httpsServer;
-	private router: any = {};
+	private httpsServerOptions: ServerOptions;
+	private router: any;
 	// Different handlers to be used depending on method
-	private crudHandlers: MethodHandlers = {
+	private crudHandlers: MethodHandler = {
 		post: null,
 		put: null,
 		get: null,
 		delete: null
 	};
-	private notFound;
+	private notFound: any;
 
 	constructor() {
+		this.init();
 	};
 
-	public init() {
+	public init(): void {
 		this.setupRoutes();
 		this.createServers();
-		this.makeServerListen();
+		this.makeServersListen();
 	}
 
-	private createServers() {
+	private createServers(): void {
 		this.httpServer = http.createServer(this.createServerCB.bind(this)); //// Instantiate http server
 		this.httpsServerOptions = {
 			'key' : fs.readFileSync('../../https/key.pem'),
@@ -51,7 +51,7 @@ export class AppServer {
 		this.httpsServer = https.createServer(this.httpsServerOptions, this.createServerCB.bind(this)); //// Instantiate httpsServer
 	}
 
-	private makeServerListen() {
+	private makeServersListen(): void {
 		// Start the http Server
 		this.httpServer.listen(3000, () => {
 			console.log("Http Server Running on Port: 3000");
@@ -63,9 +63,10 @@ export class AppServer {
 		});
 	}
 
-	private setupRoutes() {
+	private setupRoutes(): void {
 		// Determine method and call proper handler
-		this.router.hello = (data, callback) => {
+		this.router = this.router && typeof(this.router) =='object' ? this.router : {};
+		this.router.hello = (data: RequestData, callback) => {
 			let allowedMethods = ['post', 'get', 'put', 'delete'];
 			if(allowedMethods.indexOf(data.method) > -1) {
 				this.crudHandlers[data.method](data, callback);
@@ -74,43 +75,35 @@ export class AppServer {
 			}
 		};
 
-		this.crudHandlers.post = function(data, callback) {
+		this.crudHandlers.post = function(data: RequestData, callback) {
 			callback(200, {"Message": "You made a POST request to /hello. Welcome!!"});
 		};
-
-		this.crudHandlers.get = function(data, callback) {
+		this.crudHandlers.get = function(data: RequestData, callback) {
 			callback(200, {"Message": "You made a GET request to /hello. Welcomes are only for POSTs!!"});
 		};
-
-		this.crudHandlers.put = function(data, callback) {
+		this.crudHandlers.put = function(data: RequestData, callback) {
 			callback(200, {"Message": "You made a PUT request to /hello. Welcomes are only for POSTs!!"});
 		};
-
-		this.crudHandlers.delete = function(data, callback) {
+		this.crudHandlers.delete = function(data: RequestData, callback) {
 			callback(200, {"Message": "You made a DELETE request to /hello. Welcomes are only for POSTs!!"});
 		};
-
-		this.notFound = function (data, callback) {
+		this.notFound = function (data: RequestData, callback) {
 			callback(404);
 		};
 		
 	}
 	
 
-	private createServerCB(req, res) {
+	private createServerCB(req: IncomingMessage, res: ServerResponse): void {
 		// Get the url and parse it
-		let parsedUrl = url.parse(req.url, true);
-		
+		let parsedUrl = url.parse(req.url, true);		
 		// Get the path from Url
 		let path = parsedUrl.pathname;
 		let trimmedPath = path.replace(/^\/+|\/+$/g, '');
-
 		// Get the query string and parse as an object
 		let queryStringObj = parsedUrl.query;
-
 		// Get Request method
 		let method = req.method.toLowerCase();
-
 		// Get the headers 
 		let headers = req.headers;
 
@@ -161,160 +154,7 @@ export class AppServer {
 }
 
 let app = new AppServer();
-app.init();
 
 
 
 
-
-
-//Callback function used when creating either server (http & https), parses data from request and packages to be consumed by appropriate route handler, which is called
-
-
-
-
-
-
-// /// <reference path="../ref/index.d.ts" />
-// /*
-// ** Entry Point for App, starts up the server
-// */
-
-// // Dependencies
-// import * as http from 'http';
-// import * as https from 'https';
-// import * as fs from 'fs';
-// import * as url from 'url';
-// import * as String_Decoder from 'string_decoder';
-// const StringDecoder = String_Decoder.StringDecoder;
-
-// //Interfaces
-// import { MethodHandlers } from './interfaces';
-// import { RequestData } from './interfaces';
-
-// export class AppServer {
-	
-// }
-// // Instantiate http server
-// const httpServer = http.createServer(createServerCB);
-
-// // Start the http Server
-// httpServer.listen(3000, () => {
-// 	console.log("Http Server Running on Port: 3000");
-// });
-
-
-// // Instantiate httpsServer
-// const httpsServerOptions = {
-// 	'key' : fs.readFileSync('../../https/key.pem'),
-// 	'cert' : fs.readFileSync('../../https/cert.pem')
-// };
-// const httpsServer = https.createServer(httpsServerOptions, createServerCB);
-
-// // Start the httpsServer
-// httpsServer.listen(5000, () => {
-// 	console.log("Https Server Running on Port: 5000");
-// });
-
-// //Instantiate handlers
-// let router: any = {};
-
-// // Determine method and call proper handler
-// router.hello = function(data, callback) {
-// 	let allowedMethods = ['post', 'get', 'put', 'delete'];
-// 	if(allowedMethods.indexOf(data.method) > -1) {
-// 		crudHandlers[data.method](data, callback);
-// 	} else {
-// 		callback(405);
-// 	}
-// };
-
-// // Different handlers to be used depending on method
-// let crudHandlers: MethodHandlers = {
-// 	post: null,
-// 	put: null,
-// 	get: null,
-// 	delete: null
-// };
-
-// crudHandlers.post = function(data, callback) {
-// 	callback(200, {"Message": "You made a POST request to /hello. Welcome!!"});
-// };
-
-// crudHandlers.get = function(data, callback) {
-// 	callback(200, {"Message": "You made a GET request to /hello. Welcomes are only for POSTs!!"});
-// };
-
-// crudHandlers.put = function(data, callback) {
-// 	callback(200, {"Message": "You made a PUT request to /hello. Welcomes are only for POSTs!!"});
-// };
-
-// crudHandlers.delete = function(data, callback) {
-// 	callback(200, {"Message": "You made a DELETE request to /hello. Welcomes are only for POSTs!!"});
-// };
-
-// var notFound = function (data, callback) {
-// 	callback(404);
-// };
-
-// //Callback function used when creating either server (http & https), parses data from request and packages to be consumed by appropriate route handler, which is called
-// function createServerCB(req, res) {
-// 	// Get the url and parse it
-// 	let parsedUrl = url.parse(req.url, true);
-	
-// 	// Get the path from Url
-// 	let path = parsedUrl.pathname;
-// 	let trimmedPath = path.replace(/^\/+|\/+$/g, '');
-
-// 	// Get the query string and parse as an object
-// 	let queryStringObj = parsedUrl.query;
-
-// 	// Get Request method
-// 	let method = req.method.toLowerCase();
-
-// 	// Get the headers 
-// 	let headers = req.headers;
-
-// 	// Get the payload if it exists
-// 	let decoder = new StringDecoder('utf8');
-// 	let buffer = '';
-// 	req.on('data', (data) => {
-// 		buffer += decoder.write(data);		
-// 	});
-// 	req.on('end', () => {
-// 		buffer += decoder.end();
-
-// 		// Construct data object for use by handler
-// 		let data: RequestData = {
-// 			'trimmedPath' : trimmedPath,
-// 			'queryStringObj' : queryStringObj,
-// 			'method' : method,
-// 			'headers' : headers,
-// 			'payload' : buffer
-// 		};
-
-// 		// if path isn't to hello, return 404
-// 		if(trimmedPath == 'hello') {
-// 			router.hello(data, (statusCode: number, payload) : void => {
-// 				//Sanity Checks
-// 				statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
-// 				payload = typeof(payload) == 'object' ? payload : {};
-// 				//Convert payload to a string
-// 				let payloadString = JSON.stringify(payload);
-// 				// Return Response
-// 				res.setHeader('Content-Type', 'application/json');
-// 				res.writeHead(statusCode);
-// 				res.end(payloadString);
-// 			});
-// 		} else {
-// 			notFound(data, (statusCode: number) : void => {
-// 				// Return Response
-// 				res.setHeader('Content-Type', 'application/json');
-// 				res.writeHead(statusCode);
-// 				let msg = {"Message" :"We Only welcome when people come through the right door."};
-// 				res.end(JSON.stringify(msg));
-// 			});
-// 		}
-
-// 	});
-// }/
--- typescript/src/interfaces.ts
+++ typescript/src/interfaces.ts
@@ -1,8 +1,8 @@
-export interface MethodHandlers {
-	get(data: any, callback: any) : void; 
-	put(data: any, callback: any) : void; 
-	post(data: any, callback: any) : void; 
-	delete(data: any, callback: any) : void; 
+export interface MethodHandler {
+	get(data: RequestData, callback: any) : void; 
+	put(data: RequestData, callback: any) : void; 
+	post(data: RequestData, callback: any) : void; 
+	delete(data: RequestData, callback: any) : void; 
 };
 
 export interface RequestData {
